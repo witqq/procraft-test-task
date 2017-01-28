5ebfa37a6da38c8baafd24402ead3c7d690eeb3f
whitemq <golyi91@mail.ru>
2017-01-28 18:25:42 +0500

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React = require('react');
-
 import Component = React.Component;
 
 export class App extends Component<{},{}> {
     render() {
-        const e = 1;
-        if (e) {
-            throw new Error("asdasdasd");
-        }
         return <div>
             Hello world!
         </div>
